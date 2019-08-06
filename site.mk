a4c44dc3ceadf582399ae93245523933eeb286bc
Vogelbecker <info@vogelbecker.de>
2019-08-06 07:48:45 +0200

    update for firmeware 0.7.6
    update for firmeware 0.7.6

--- site.mk
+++ site.mk
@@ -1,61 +1,62 @@
-##	gluon site.mk makefile example
-
-##	GLUON_SITE_PACKAGES
-#		specify gluon/openwrt packages to include here
-#		The gluon-mesh-batman-adv-* package must come first because of the dependency resolution
+##      gluon site.mk makefile example
+##      GLUON_SITE_PACKAGES
+#               specify gluon/openwrt packages to include here
+#               The gluon-mesh-batman-adv-* package must come first because of the dependency resolution
 
 GLUON_SITE_PACKAGES := \
-	gluon-mesh-batman-adv-15 \
-	gluon-respondd \
-	gluon-alfred \
-	gluon-autoupdater \
-	gluon-config-mode-core \
-	gluon-config-mode-hostname \
-	gluon-config-mode-autoupdater \
-	gluon-config-mode-mesh-vpn \
-	gluon-config-mode-geo-location \
-	gluon-config-mode-contact-info \
-	gluon-ebtables-filter-multicast \
-	gluon-ebtables-filter-ra-dhcp \
-	gluon-web-admin \
-	gluon-web-autoupdater \
-	gluon-web-private-wifi \
-	gluon-web-wifi-config \
-	gluon-neighbour-info \
-	gluon-mesh-vpn-tunneldigger \
-	gluon-radvd \
-	gluon-setup-mode \
-	gluon-status-page \
-	gluon-dns-cache \ 
-	respondd-module-airtime \
-	iwinfo \
-	iptables \
-	haveged
-
+        gluon-mesh-batman-adv-15 \
+        gluon-authorized-keys \
+        gluon-respondd \
+        respondd-module-airtime \
+        gluon-alfred \
+        gluon-autoupdater \
+        gluon-config-mode-hostname \
+        gluon-config-mode-autoupdater \
+        gluon-config-mode-mesh-vpn \
+        gluon-config-mode-geo-location \
+        gluon-config-mode-contact-info \
+        gluon-ebtables-filter-multicast \
+        gluon-ebtables-filter-ra-dhcp \
+        gluon-ebtables-limit-arp \
+        gluon-web-admin \
+        gluon-web-autoupdater \
+        gluon-web-network \
+        gluon-web-private-wifi \
+        gluon-web-wifi-config \
+        gluon-neighbour-info \
+        gluon-mesh-vpn-tunneldigger \
+        gluon-radvd \
+        gluon-status-page \
+        iwinfo \
+        iw-full \
+        iptables \
+        gluon-rsk-config \
+        gluon-rsk-block-mesh \
+        haveged
 # add offline ssid only if the target has wifi device
 ifeq ($(GLUON_TARGET),ar71xx-generic)
 GLUON_SITE_PACKAGES += \
-	gluon-ssid-changer
+        gluon-ssid-changer
 endif
 
 ifeq ($(GLUON_TARGET),ar71xx-mikrotik)
 GLUON_SITE_PACKAGES += \
-	gluon-ssid-changer
+        gluon-ssid-changer
 endif
 
 ifeq ($(GLUON_TARGET),ar71xx-nand)
 GLUON_SITE_PACKAGES += \
-	gluon-ssid-changer
+        gluon-ssid-changer
 endif
 
 ifeq ($(GLUON_TARGET),mpc85xx-generic)
 GLUON_SITE_PACKAGES += \
-	gluon-ssid-changer
+        gluon-ssid-changer
 endif
 
 ifeq ($(GLUON_TARGET),ramips-rt305x)
 GLUON_SITE_PACKAGES += \
-	gluon-ssid-changer
+        gluon-ssid-changer
 endif
 
 # add addition network drivers and usb stuff only to targes where disk space does not matter.
@@ -68,12 +69,7 @@ GLUON_SITE_PACKAGES += \
         kmod-usb-net \
         kmod-usb-net-asix \
         kmod-usb-net-dm9601-ether \
-        kmod-sky2 \
-        kmod-r8169 \
-        kmod-forcedeth \
-        kmod-8139too \
-	kmod-atl2 \
-	kmod-igb
+        kmod-igb
 endif
 
 ifeq ($(GLUON_TARGET),x86-64)
@@ -85,12 +81,7 @@ GLUON_SITE_PACKAGES += \
         kmod-usb-net \
         kmod-usb-net-asix \
         kmod-usb-net-dm9601-ether \
-        kmod-sky2 \
-        kmod-r8169 \
-        kmod-forcedeth \
-        kmod-8139too \
-	kmod-atl2 \
-	kmod-igb
+        kmod-igb
 endif
 
 # Add offline ssid, network drivers and usb stuff to raspberry and banana pi images
@@ -103,12 +94,7 @@ GLUON_SITE_PACKAGES += \
         kmod-usb-hid \
         kmod-usb-net \
         kmod-usb-net-asix \
-        kmod-usb-net-dm9601-ether \
-        kmod-sky2 \
-        kmod-r8169 \
-        kmod-forcedeth \
-        kmod-8139too \
-        kmod-atl2
+        kmod-usb-net-dm9601-ether
 endif
 
 ifeq ($(GLUON_TARGET),brcm2708-bcm2709)
@@ -119,12 +105,7 @@ GLUON_SITE_PACKAGES += \
         kmod-usb-hid \
         kmod-usb-net \
         kmod-usb-net-asix \
-        kmod-usb-net-dm9601-ether \
-        kmod-sky2 \
-        kmod-r8169 \
-        kmod-forcedeth \
-        kmod-8139too \
-        kmod-atl2
+        kmod-usb-net-dm9601-ether
 endif
 
 ifeq ($(GLUON_TARGET),sunxi)
@@ -135,31 +116,26 @@ GLUON_SITE_PACKAGES += \
         kmod-usb-hid \
         kmod-usb-net \
         kmod-usb-net-asix \
-        kmod-usb-net-dm9601-ether \
-        kmod-sky2 \
-        kmod-r8169 \
-        kmod-forcedeth \
-        kmod-8139too \
-        kmod-atl2
+        kmod-usb-net-dm9601-ether
 endif
 
 
-##	DEFAULT_GLUON_RELEASE
-#		version string to use for images
-#		gluon relies on
-#			opkg compare-versions "$1" '>>' "$2"
-#		to decide if a version is newer or not.
+##      DEFAULT_GLUON_RELEASE
+#               version string to use for images
+#               gluon relies on
+#                       opkg compare-versions "$1" '>>' "$2"
+#               to decide if a version is newer or not.
 
-DEFAULT_GLUON_RELEASE := 0.4.0
+DEFAULT_GLUON_RELEASE := 0.7.6
 
-DEFAULT_GLUON_CHECKOUT := v2017.1.x
+DEFAULT_GLUON_CHECKOUT := v2018.2.x
 
-##	GLUON_RELEASE
-#		call make with custom GLUON_RELEASE flag, to use your own release version scheme.
-#		e.g.:
-#			$ make images GLUON_RELEASE=23.42+5
-#		would generate images named like this:
-#			gluon-ff%site_code%-23.42+5-%router_model%.bin
+##      GLUON_RELEASE
+#               call make with custom GLUON_RELEASE flag, to use your own release version scheme.
+#               e.g.:
+#                       $ make images GLUON_RELEASE=23.42+5
+#               would generate images named like this:
+#                       gluon-ff%site_code%-23.42+5-%router_model%.bin
 
 # Allow overriding the release number from the command line
 GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
@@ -174,4 +150,4 @@ GLUON_REGION ?= eu
 GLUON_LANGS ?= en de
 
 # Select ath10k Firmware for adhoc
-GLUON_ATH10K_MESH ?= ibss
+GLUON_ATH10K_MESH ?= 11s
